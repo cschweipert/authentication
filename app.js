b48c78be8a25016d705e1202e1b3923f492ff747
Christina Schweipert <c.schweipert@gmail.com>
2020-02-04 15:55:28 -0700

    install dotenc and add environment variables

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();  //has to right on the top
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -20,8 +21,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisismylittlesecret.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
