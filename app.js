41130c6f8c2eeb24a95d858e226af604d17e5d04
Christina Schweipert <c.schweipert@gmail.com>
2020-02-04 16:30:25 -0700

    install and use md5 to hash password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const md5 = require("md5");
 
 const app = express();
 
@@ -38,7 +38,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
